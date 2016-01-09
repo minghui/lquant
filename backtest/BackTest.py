51b42784d3be535fe2d0d60090734bab125f0189
squall1988 <lonelydragon1988@gmail.com>
2016-01-09 23:24:26 +0800

    Merge branch 'master' of github.com:squall1988/lquant
    Conflicts:
    	backtest/BackTest.py
    	backtest/utils/mysql.py

--- backtest/BackTest.py
+++ backtest/BackTest.py
@@ -49,53 +49,25 @@ class BackTestBase(object):
         # Use database to store data.
         database_config = config['database']
         if database_config['name'] == 'mysql':
-            self._database = DBUtil(database_config['user'],
-                                    str(database_config['passwd']),
-                                    database_config['database'])
+            self._database = MySQLUtils(database_config['user'],
+                                        str(database_config['passwd']),
+                                        database_config['database'])
         elif database_config['name'] == 'rds':
             self._database = RDSDB(self.logger)
             self._db_type = database_config['type']
         self._summary = {}
         self._strategy = None
-        # for every stock we have a container
-        self.stock_asset = RecordContainer()
         # Use to save the everydays return and loss
         self.asset_dict = {}
         self.asset_daliy = []
         self._analysis = None
         self._trade_strategy = None
 
-    def init(self, strategy=None, analysis=None):
+    def init(self, strategy=None, trade_strategy=None, analysis=None):
         self._strategy = strategy
+        self._strategy.set_data_need(DATA_NEED[self._db_type])
         self._analysis = analysis
-
-    def buy_strategy(self, name, data):
-        """
-        Here is just a sample buy strategy.
-        :param data:
-        :return:
-        """
-        number = np.floor(self._fund/(data[4]*100))
-        self._fund -= data[4]*number*100
-        if number != 0:
-            record = Record(name=name, date=data[1],
-                            number=number, price=data[4], tax=0, buy=True)
-            self.stock_asset.add_record(record)
-            return record
-
-    def sell_strategy(self, name, data):
-        """
-        Here is just a sample sell strategy.
-        # TODO: Sell strategy should be configable.
-        :param data:
-        :return:
-        """
-        record = self.stock_asset.get_record(name)
-        sell_record = Record(name=name, date=data[1],
-                             price=data[4], number=record.number, tax=0, sell=True)
-        self._fund += record.number*data[4]*100
-        self.stock_asset.add_record(sell_record)
-        self._sell_record_list.append(sell_record)
+        self._trade_strategy = trade_strategy
 
     def set_strategy(self, strategy):
         self._strategy = strategy
@@ -126,31 +98,7 @@ class BackTestBase(object):
             # print 'This is the shape of the stock', stock_data.shape
             self._test_strategy(stock, stock_data)
 
-    def _test_strategy(self, stock, stock_data):
-        needed_data_length = self._strategy.get_data_need()
-        asset_daliy = []
-        for i in range(needed_data_length, stock_data.shape[0]):
-            tmp_data = stock_data[(i-needed_data_length):i]
-            if_buy = self._strategy.if_buy(tmp_data[-1])
-            if if_buy:
-                record = self.buy_strategy(stock, tmp_data[-1])
-                if record is not None:
-                    self._buy_record_list.append(record)
-                    self.get_assert(stock, tmp_data[-1])
-                    continue
-            if_sell = self._strategy.if_sell(tmp_data[-1])
-            if if_sell:
-                record = self.sell_strategy(stock, tmp_data[-1])
-                if record is not None:
-                    self._sell_record_list.append(record)
-            self.get_assert(stock, tmp_data[-1])
-        date_index = stock_data[needed_data_length:, 1]
-        result = pd.DataFrame(data=self.asset_daliy, index=date_index, columns=["return"])
-        self._summary[stock] = self._strategy.summary()
-        self.asset_dict.update({stock: result})
-
     def summary(self):
-        print self.asset_dict
         for x in self.asset_dict:
             # print self.asset_dict[x]
             self.get_benchmark()
@@ -165,18 +113,39 @@ class BackTestBase(object):
                 self._analysis(result)
             result.plot()
             plt.show()
-        print self._buy_record_list
-        print self._sell_record_list
-        return self._summary
 
     def get_stock_asset(self):
         pass
-        # recordself.stock_asset.get_record(stock)
 
-    def get_assert(self, stock, data):
-        record = self.stock_asset.get_record(stock)
-        current_asset = self._fund + record.number*data[4]*100
-        self.asset_daliy.append(current_asset)
+    def get_benchmark(self):
+        """
+        Get the benchmark. Used to compare with strategy.
+        # """
+        # if self._db_type == 'minute':
+        #     benchmark_data = self._database.get_array(self._benchmark_name,
+        #                                               self._begin_date,
+        #                                               self._end_date,
+        #                                               m=1)
+        # elif self._db_type == 'day':
+        benchmark_data = self._database.get_array(self._benchmark_name,
+                                                  self._begin_date,
+                                                  self._end_date,
+                                                  m=1440)
+        # elif self._db_type == '5minute':
+        #     benchmark_data = self._database.get_array(self._benchmark_name,
+        #                                               self._begin_date,
+        #                                               self._end_date,
+        #                                               m=5)
+        # else:
+        #     raise ValueError("Do not support type")
+
+        date_index = np.array([x.split(' ')[0] for x in benchmark_data[1:, 0]])
+        benchmark_data = benchmark_data[:, 4]
+        change_rate = (benchmark_data[1:] - benchmark_data[0]) / benchmark_data[
+            0]
+        self._benchmark_data = pd.DataFrame(data=change_rate, index=date_index,
+                                            columns=["benchmark"])
+        print self._benchmark_data
 
     def _init_assert(self):
         """
@@ -184,17 +153,37 @@ class BackTestBase(object):
         """
         self._fund = self._base_fund
         self._strategy.init(self._fund)
-        self.asset_daliy = []
+        self._trade_strategy.init(self._fund)
 
-    def get_benchmark(self):
-        """
-        Get the benchmark. Used to compare with strategy.
-        """
-        benchmark_data = self._database.get_array(self._benchmark_name, self._begin_date, self._end_date)
-        date_index = benchmark_data[1:, 1]
-        benchmark_data = benchmark_data[:, 4]
-        change_rate = (benchmark_data[1:] - benchmark_data[0])/benchmark_data[0]
-        self._benchmark_data = pd.DataFrame(data=change_rate, index=date_index, columns=["benchmark"])
+    def _test_strategy(self, stock, stock_data):
+        # print 'This is the stock_data', stock_data
+        needed_data_length = self._strategy.get_data_need()
+        for tmp_data in stock_data:
+            buy_price = self._strategy.if_buy(tmp_data)
+            if buy_price is not None:
+                record = self._trade_strategy.buy_strategy(name=stock,
+                                                           price=buy_price)
+                if record is not None:
+                    self._buy_record_list.append(record)
+                    self._trade_strategy.get_assert(stock, tmp_data[-1])
+                    continue
+            sell_price = self._strategy.if_sell(tmp_data)
+            if sell_price is not None:
+                record = self._trade_strategy.sell_strategy(name=stock,
+                                                            price=sell_price)
+                if record is not None:
+                    self._sell_record_list.append(record)
+            self._trade_strategy.get_assert(stock, tmp_data[-1])
+        print 'This is stock data', stock_data
+        date_index = [x[0] for x in stock_data]
+        print stock_data
+        print date_index
+        result = pd.DataFrame(data=self._trade_strategy.asset_daliy,
+                              index=date_index,
+                              columns=["return"])
+        print result
+        self._summary[stock] = self._strategy.summary()
+        self.asset_dict.update({stock: result})
 
 
 class TradeStrategy(object):
@@ -210,10 +199,28 @@ class TradeStrategy(object):
         self._sell_record_list = []
         self._buy_record_list = []
 
-class SellStrategy(object):
-    """
-    This is the sell strategy base class.
-    """
+    def init(self, fund):
+        self._fund = fund
+        self.stock_asset.clear()
+        self.asset_daliy = []
+
+    def buy_strategy(self, *argv, **kwargs):
+        """
+        Here is just a sample buy strategy.
+        :param data:
+        :return:
+        """
+        name = kwargs.get('name')
+        price = kwargs.get("price")
+        date = kwargs.get("date")
+        number = np.floor(self._fund / (price * 100))
+        if number != 0:
+            self._fund -= price * number * 100
+            record = Record(name=name, date=date,
+                            number=number, price=price, tax=0, buy=True)
+            self.stock_asset.add_record(record)
+            return record
+
     def sell_strategy(self, *args, **kwargs):
 
         """
