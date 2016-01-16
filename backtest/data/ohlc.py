3eedcd63db6482c797a97b4c034becb3652f3bf6
squall1988 <lonelydragon1988@gmail.com>
2016-01-16 20:27:37 +0800

    Add ohlc method. Add test.

--- backtest/data/ohlc.py
+++ backtest/data/ohlc.py
@@ -4,6 +4,7 @@ __author__ = 'squall'
 import numpy as np
 import pandas as pd
 import talib
+# from backtest.utils.mysql import MySQLUtils
 
 """
 This file contain all the method used for parsing OHLC data.
@@ -29,6 +30,13 @@ class OHLCVD(object):
             raise ValueError("Do not support type")
         if self.data.shape[1] != 7:
             raise ValueError("Wrong shape of the data")
+        self._inputs = {
+            'open': data[:, 1].astype(np.float64),
+            'high': data[:, 2].astype(np.float64),
+            'low': data[:, 3].astype(np.float64),
+            'close': data[:, 4].astype(np.float64),
+            'volume': np.exp(data[:, 5].astype(np.float64))
+        }
 
     def current_time(self):
         return self.data[-1, 0]
@@ -48,3 +56,16 @@ class OHLCVD(object):
             pass
 
         pass
+
+    def add_macd(self):
+        macd_result = talib.abstract.MACD(self._inputs)
+        macd_result = np.vstack(macd_result).T
+        macd_result = np.hstack((self.data, macd_result))
+        self.dataframe = pd.DataFrame(data=macd_result,
+                                      columns=["date", "open", "high", "low", "close", "volume",
+                                               "deal", "dea", "dma", "dif"],
+                                      index=self.dataframe.date)
+        self.data = self.dataframe.values
+        return True
+
+
---
+++ test/test_mysql.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+__author__ = 'squall'
+
+from backtest.utils.mysql import MySQLUtils
+from backtest.data.ohlc import OHLCVD
+
+if __name__ == '__main__':
+    db = MySQLUtils('root', '1988', 'test', 'stock')
+    result = db.get_array('sh600741', begin='2015-10-10', end='2015-12-12')
+    ohlc = OHLCVD(data=result)
+    ohlc.add_macd()
+    ohlc.dataframe.plot()
+    from matplotlib import pylab as plt
+    plt.show()
