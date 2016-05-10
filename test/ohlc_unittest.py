7b54a08a1ed7f2b5cc2e99f37159f55cb60e5bb0
squall1988 <lonelydragon1988@gmail.com>
2016-05-10 22:53:23 +0800

    Add all usable feature.

--- test/ohlc_unittest.py
+++ test/ohlc_unittest.py
@@ -6,15 +6,13 @@ from data.ohlc import  OHLCVD
 
 
 class MyTestCase(unittest.TestCase):
+
     def test_ohlc_add_all_feature(self):
         data = pd.read_csv('d:/stock/new-data/SH600741.TXT', sep='\t')
-        print data.columns
         ohlc = OHLCVD(data.values)
         ohlc.add_all_ta_feature()
         data = ohlc.get_dataframe()
-        print data.columns
-
-        # self.assertEqual(True, False)
+        self.assertEqual(ohlc.data.shape[1], 149)
 
 
 if __name__ == '__main__':
