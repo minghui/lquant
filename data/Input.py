6d6199b34161364846ee7d1eb2c9943c8a5d09b4
squall1988 <lonelydragon1988@gmail.com>
2016-05-15 14:33:21 +0800

    Add input class.

---
+++ data/Input.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+
+class Data(object):
+
+    def __init__(self):
+        self.data = None
+        self.data_frame = None
+
+    def get_data(self):
+        return self.data
+
+    def get_dataframe(self):
+        return self.data_frame
